1727f253615fc82e8edfac7173456d35a5306ddf
Hermanstray <hermanstraye@gmail.com>
2023-04-22 15:28:16 +0200

    Til Even

--- raspi_analyze (2).py
+++ raspi_analyze (2).py
@@ -5,6 +5,7 @@ import scipy.signal as signal
 import estimateAngle
 
 
+
 def bandpass_filter(data, lowcut, highcut, fs, order=5):
     nyquist = 0.5 * fs
     low = lowcut / nyquist
@@ -28,11 +29,11 @@ def raspi_import(path, channels=5):
 Fs = 31500 #Hz
 
 def maxCorrelationLag(signal1, signal2):
-    correlatedSignal = abs(np.correlate(signal1, signal2, "full"))
+    correlatedSignal = abs(signal.correlate(signal1, signal2, "full", "auto"))
     return correlatedSignal
 
 def maxLag(signal1, signal2):
-    correlatedSignal = abs(np.correlate(signal1, signal2, "full"))
+    correlatedSignal = abs(signal.correlate(signal1, signal2, "full", "auto"))
     maxLag = np.argmax(correlatedSignal) - ((len(correlatedSignal)-1)/2)
     print("maxlag:", maxLag)
     return maxLag
@@ -64,6 +65,8 @@ maxCorrLag21 = maxCorrelationLag(filteredSignalMic2, filteredSignalMic1)
 maxCorrLag31 = maxCorrelationLag(filteredSignalMic3, filteredSignalMic1)  
 maxCorrLag32 = maxCorrelationLag(filteredSignalMic3, filteredSignalMic2)  
 
+autokorr11 = maxCorrelationLag(filteredSignalMic1, filteredSignalMic1)
+
 maxLag21 = maxLag(filteredSignalMic2, filteredSignalMic1)   
 maxLag31 = maxLag(filteredSignalMic3, filteredSignalMic1)  
 maxLag32 = maxLag(filteredSignalMic3, filteredSignalMic2) 
@@ -71,28 +74,29 @@ maxLag32 = maxLag(filteredSignalMic3, filteredSignalMic2)
 angle = estimateAngle.estimateAngleByLMS(maxLag21, maxLag31, maxLag32)
 print("Angle:", angle)
 
-freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
-freq = np.fft.fftshift(freq)
-spectrum = np.fft.fftshift(np.fft.fft(data, axis=0), axes=0)
+# freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
+# freq = np.fft.fftshift(freq)
+# spectrum = np.fft.fftshift(np.fft.fft(data, axis=0), axes=0)
 
-time = np.arange(len(filteredSignalMic1)-1) / Fs
+time = np.arange(0, len(filteredSignalMic1)) / Fs
+#time = [-len(time)//2, len(time)//2]
 
 timeFig, timeAxs = plt.subplots(3,1,figsize =(10,8))
 
 #timeAxs[0].subplot(3,1,1)
-timeAxs[0].plot(time, filteredSignalMic1[1:], color = 'green')
+timeAxs[0].plot(time, filteredSignalMic1, color = 'green')
 timeAxs[0].set_title('Tidssignal Mikrofon 1')
 timeAxs[0].set_xlabel('Tid [s]')
 timeAxs[0].set_ylabel('Spenning [mV]')
 
 #timeAxs[1].subplot(3,1,2)
-timeAxs[1].plot(time, filteredSignalMic2[1:], color = 'red')
+timeAxs[1].plot(time, filteredSignalMic2, color = 'red')
 timeAxs[1].set_title('Tidssignal Mikrofon 2')
 timeAxs[1].set_xlabel('Tid [s]')
 timeAxs[1].set_ylabel('Spenning [mV]')
 
 #timeAxs[2].subplot(3,1,3)
-timeAxs[2].plot(time, filteredSignalMic3[1:], color = 'blue')
+timeAxs[2].plot(time, filteredSignalMic3, color = 'blue')
 timeAxs[2].set_title('Tidssignal Mikrofon 3')
 timeAxs[2].set_xlabel('Tid [s]')
 timeAxs[2].set_ylabel('Spenning [mV]')
@@ -101,27 +105,54 @@ plt.tight_layout()
 plt.show()
 
 # print("maxcorrlag21", maxCorrLag21[1], " length", len(maxCorrLag21[1]))
+#[((len(maxCorrLag32)//2)+1):(len(maxCorrLag32)//2)+100]
+corrLength = np.arange((-len(maxCorrLag21)//2)+1, (len(maxCorrLag21))//2)
 
-corrLength = np.arange(99)
+fig, axs = plt.subplots(figsize=(10, 8))
+# fig.subplots_adjust(hspace=0.5)
 
-fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+axs.plot(corrLength, maxCorrLag21[:len(maxCorrLag21)-1], color='green', label = 'Krysskorrelasjon Mikrofon 21')
+axs.plot(corrLength, maxCorrLag31[:len(maxCorrLag21)-1], color='red', label = 'Krysskorrelasjon Mikrofon 31')
+axs.plot(corrLength, maxCorrLag32[:len(maxCorrLag21)-1], color='blue', label = 'Krysskorrelasjon Mikrofon 32')
+axs.set_title('Krysskorrelasjon')
+axs.set_xlabel('Samples')
+axs.set_ylabel('Signal amplitude [-]')
 
-axs[0].plot(corrLength, maxCorrLag21[((len(maxCorrLag21)//2)+1):(len(maxCorrLag21)//2)+100], color='green')
-axs[0].set_title('Autokorrelasjon Mikrofon 21')
-axs[0].set_xlabel('Tid [s]')
-axs[0].set_ylabel('Signal amplitude [-]')
+plt.xlim(-20,20)
+plt.legend()
+plt.show()
 
-axs[1].plot(corrLength, maxCorrLag31[((len(maxCorrLag31)//2)+1):(len(maxCorrLag31)//2)+100], color='red')
-axs[1].set_title('Autokorrelasjon Mikrofon 31')
-axs[1].set_xlabel('Tid [s]')
-axs[1].set_ylabel('Signal amplitude [-]')
 
-axs[2].plot(corrLength, maxCorrLag32[((len(maxCorrLag32)//2)+1):(len(maxCorrLag32)//2)+100], color='blue')
-axs[2].set_title('Autokorrelasjon Mikrofon 32')
-axs[2].set_xlabel('Tid [s]')
-axs[2].set_ylabel('Signal amplitude [-]')
 
-plt.tight_layout()
+# Upsample the signals by a factor of 8 using resample_poly
+upsampling_factor = 8
+downsampling_factor = 1
+upsampled_signal21 = signal.resample_poly(maxCorrLag21,  len(maxCorrLag21) * upsampling_factor, len(maxCorrLag21) * downsampling_factor)
+upsampled_signal31 = signal.resample_poly(maxCorrLag31,  len(maxCorrLag21) * upsampling_factor, len(maxCorrLag21) * downsampling_factor)
+upsampled_signal32 = signal.resample_poly(maxCorrLag32,  len(maxCorrLag21) * upsampling_factor, len(maxCorrLag21) * downsampling_factor)
+
+xaks = np.arange(-len(upsampled_signal21)//2, len(upsampled_signal21)//2)
+
+fig, axs = plt.subplots()
+axs.plot(xaks, upsampled_signal21[:len(upsampled_signal21)], color='green', label = 'Krysskorrelasjon Mikrofon 21')
+axs.plot(xaks, upsampled_signal31[:len(upsampled_signal31)], color='red', label = 'Krysskorrelasjon Mikrofon 31')
+axs.plot(xaks, upsampled_signal32[:len(upsampled_signal32)], color='blue', label = 'Krysskorrelasjon Mikrofon 32')
+axs.set_title('Krysskorrelasjon med oppsamplingsfaktor = 8')
+axs.set_xlabel('Samples')
+axs.set_ylabel('Signal amplitude [-]')
+
+plt.xlim(-20,70)
+plt.legend()
+plt.show()
+
+
+
+plt.plot(corrLength, autokorr11[:len(maxCorrLag21)-1])
+plt.gca().set_title('Autokorrelasjon Mikrofon 1')
+plt.gca().set_xlabel('Samples')
+plt.gca().set_ylabel('Signal amplitude [-]')
+plt.gca().set_xlim(-20,20)
+
 plt.show()
 
 # plt.plot(freq[len(freq)//2:], 20*np.log10(np.abs(spectrum[len(freq)//2:])))
